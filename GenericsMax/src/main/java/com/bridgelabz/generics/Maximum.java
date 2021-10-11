7a74512a5777c0bb21135d2c7105b16987fc646e
prashant169 <prashantphad2018@gmail.com>
2021-10-11 01:49:23 -0800

    Program To Find Max in 3 String

--- GenericsMax/src/main/java/com/bridgelabz/generics/Maximum.java
+++ GenericsMax/src/main/java/com/bridgelabz/generics/Maximum.java
@@ -1,8 +1,8 @@
 package com.bridgelabz.generics;
 
 public class Maximum {
-	public static Float getmaximum(Float value1, Float value2, Float value3) {
-		Float max = value1;
+	public static String getmaximum(String value1, String value2, String value3) {
+		String max = value1;
 		if (value2.compareTo(value1) > 0 && value2.compareTo(value3) > 0)
 			max = value2;
 		else if (value3.compareTo(value2) > 0 && value3.compareTo(value1) > 0)
--- GenericsMax/src/test/java/com/bridgelabz/generics/MaximumTest.java
+++ GenericsMax/src/test/java/com/bridgelabz/generics/MaximumTest.java
@@ -5,14 +5,14 @@ import org.junit.Test;
 
 public class MaximumTest {
 	@Test
-	public void floatMax1() {
-		float actualValue = Maximum.getmaximum(12f, 14f, 15f);
-		Assert.assertEquals(15, actualValue, 0);
+	public void stringMax1() {
+		String actualValue = Maximum.getmaximum("Aa", "Bb", "Cc");
+		Assert.assertEquals("Cc", actualValue);
 	}
 
 	@Test
-	public void floatMax2() {
-		float actualValue = Maximum.getmaximum(1000f, 545f, 746f);
-		Assert.assertEquals(1000, actualValue, 0);
+	public void stringMax2() {
+		String actualValue = Maximum.getmaximum("abc", "ABC", "def");
+		Assert.assertEquals("def", actualValue);
 	}
 }
