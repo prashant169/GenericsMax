04aff0616ceb249842e3ff80f45ae5a0a5ab56cd
prashant169 <prashantphad2018@gmail.com>
2021-10-11 01:59:46 -0800

    Max method to take more thrn 3 parameters

--- GenericsMax/src/test/java/com/bridgelabz/generics/MaximumTest.java
+++ GenericsMax/src/test/java/com/bridgelabz/generics/MaximumTest.java
@@ -5,14 +5,20 @@ import org.junit.Test;
 
 public class MaximumTest {
 	@Test
-	public void stringMax1() {
-		String actualValue = Maximum.getmaximum("Aa", "Bb", "Cc");
-		Assert.assertEquals("Cc", actualValue);
+	public void intMax() {
+		int actualValue = Maximum.testMaximum(10, 20, 5);
+		Assert.assertEquals(20, actualValue);
 	}
 
 	@Test
-	public void stringMax2() {
-		String actualValue = Maximum.getmaximum("abc", "ABC", "def");
-		Assert.assertEquals("def", actualValue);
+	public void floatMax() {
+		float actualValue = Maximum.testMaximum(11f, 52f, 36f);
+		Assert.assertEquals(52f, actualValue, 0);
+	}
+
+	@Test
+	public void stringMax() {
+		String actualValue = Maximum.testMaximum("ABC", "def", "xyz");
+		Assert.assertEquals("xyz", actualValue);
 	}
 }
