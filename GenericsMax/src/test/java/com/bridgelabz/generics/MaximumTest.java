5edadb296c87bc4031362a979e9415d0eb80850a
prashant169 <prashantphad2018@gmail.com>
2021-10-11 01:34:48 -0800

    Program to find max in 3 Flots

--- GenericsMax/src/test/java/com/bridgelabz/generics/MaximumTest.java
+++ GenericsMax/src/test/java/com/bridgelabz/generics/MaximumTest.java
@@ -1,24 +1,18 @@
 package com.bridgelabz.generics;
 
-import org.junit.Test;
 import org.junit.Assert;
+import org.junit.Test;
 
 public class MaximumTest {
 	@Test
-	public void intMax() {
-		int actualValue = Maximum.testMaximum(9, 4, 1);
-		Assert.assertEquals(9, actualValue);
+	public void floatMax1() {
+		float actualValue = Maximum.getmaximum(12f, 14f, 15f);
+		Assert.assertEquals(15, actualValue, 0);
 	}
 
 	@Test
-	public void floatMax() {
-		float actualValue = Maximum.testMaximum(1.5f, 9.1f, 4.4f);
-		Assert.assertEquals(9.1f, actualValue, 0);
-	}
-
-	@Test
-	public void stringMax() {
-		String actualValue = Maximum.testMaximum("apple", "peach", "banana");
-		Assert.assertEquals("peach", actualValue);
+	public void floatMax2() {
+		float actualValue = Maximum.getmaximum(1000f, 545f, 746f);
+		Assert.assertEquals(1000, actualValue, 0);
 	}
 }
